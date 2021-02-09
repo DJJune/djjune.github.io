0d9189f04c2664fda12feb0b388ad019a14b0d25
DJJune <liangjingjun@ruc.edu.cn>
2021-02-10 01:10:47 +0800

    rm game over

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -138,7 +138,7 @@ HTMLActuator.prototype.updateBestScore = function (bestScore) {
 
 HTMLActuator.prototype.message = function (won) {
   var type    = won ? "game-won" : "game-over";
-  var message = won ? "You win!" : "Game over!";
+  var message = won ? "You win!" : "Yeah!";
 
   this.messageContainer.classList.add(type);
   this.messageContainer.getElementsByTagName("p")[0].textContent = message;
